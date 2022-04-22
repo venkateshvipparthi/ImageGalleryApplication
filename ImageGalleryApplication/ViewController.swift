2536c8345980b2c69a09b0753726a4ca4093a1e7
venkateshvipparthi <venkateshsai.vipparthi@gmail.com>
2022-04-22 13:46:29 +0100

    Initial Commit

---
+++ ImageGalleryApplication/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ImageGalleryApplication
+//
+//  Created by Admin on 22/04/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
